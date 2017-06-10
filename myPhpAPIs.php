8f01074028218c60ba8b3e32160081b7702da957
chiou <bohemianwind@gmail.com>
2017-06-11 01:37:03 +0800

    implement 'function myPrintObj()'

--- myPhpAPIs.php
+++ myPhpAPIs.php
@@ -1,3 +1,30 @@
 <?php
 
+function myPrintObj($Obj, string $terminator) {
+    $type = gettype($Obj);
+
+    switch ($type) {
+        case "string":
+            echo $Obj . $terminator;
+            break;
+
+        case "array":
+            foreach ($Obj as $key=>$v) {
+                if ( gettype($v) == "string" ) {
+                    echo "{$key} : {$v}" . $terminator;
+                } else {
+                    echo "{$key} : ";
+                    var_dump($v);
+                    echo $terminator;
+                }
+            }
+
+            break;
+
+        default:
+            var_dump($Obj);
+
+    }
+}
+
 ?>
