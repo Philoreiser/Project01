3148d0ec2cc37bc4a805e65a18671ee6ded1f2a4
chiou <bohemianwind@gmail.com>
2017-06-11 19:19:41 +0800

    import remaining seats data into MySQL database

--- dbBuildRemainingSeats.php
+++ dbBuildRemainingSeats.php
@@ -3,33 +3,41 @@
 include "pdo_sql.php";
 include "myPhpAPIs.php";
 
-$seat = array();
+// $seat = array();
 
 $handle = @fopen("./OpenData/seat_ver170610.txt","r");
-
 // $handle = @fopen("http://www.railway.gov.tw/Upload/UserFiles/seat.txt","r");
-$count = 0;
+
 if ($handle) {
-    while (($buffer = fgets($handle)) !== false ) {
-        $seat [] = $buffer;
-        $count++;
+    $pdo = @new pdo($pdo_dsn, $db_user, $db_password, $pdo_opt);
+    $sql = "INSERT INTO RemainingTickets( date, train, depStation, arrStation, tickets) VALUES (?,?,?,?,?)";
+    $stmt = $pdo->prepare($sql);
+    
+    while (($code = fgets($handle)) !== false ) {
+        // $code = $buffer;
+        // var_dump($code);
+
+        $data = myParseSeatCode($code, $mySeatFormat);
+        $stmt->execute($data);
+        // $seat [] = $buffer;
+        // $count++;
     }
 
-    // echo '<hr>';
-    // echo "count: {$count}".'<br>';
-
-    foreach ($seat as $index=>$code) {
+    // foreach ($seat as $index=>$code) {
         
-        $data = myParseSeatCode($code,$mySeatFormat);
-        echo $index .":" . '<br>';
-        var_dump($data);
-        foreach ( $data as $key=>$val) {
-            echo "{$key} : {$val} /";
-        }
-        echo '<br>';
+    //     $data = myParseSeatCode($code,$mySeatFormat);
+
+
+
+    //     // echo $index .":" . '<br>';
+    //     // var_dump($data);
+    //     // foreach ( $data as $key=>$val) {
+    //     //     echo "{$key} : {$val} /";
+    //     // }
+    //     // echo '<br>';
 
         
-    }
+    // }
 
     fclose($handle);
 }
--- pdo_sql.php
+++ pdo_sql.php
@@ -1,13 +1,13 @@
 <?php
-$host = '127.0.0.1';
-$db = 'railway';
-$user = 'root';
-$password = 'root';
-$driver = 'mysql';
-$charset = "utf8";
+$db_host = '127.0.0.1';
+$db_name = 'railway';
+$db_user = 'root';
+$db_password = 'root';
+$db_driver = 'mysql';
+$db_charset = "utf8";
 
-$dsn = "{$driver}: host = {$host}; dbname={$db}; charset={$charset}";
-$opt = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_BOTH];
+$pdo_dsn = "{$db_driver}: host = {$db_host}; dbname={$db_name}; charset={$db_charset}";
+$pdo_opt = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_BOTH];
 
 ?>
 
