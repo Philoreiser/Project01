0bd162ae85d0b34892a9b1f8edb452a86a747896
chiou <bohemianwind@gmail.com>
2017-06-12 19:24:11 +0800

    Try to load data from database 'StationsInfo'

--- dbBuildTrainScheduleInfo.php
+++ dbBuildTrainScheduleInfo.php
@@ -2,7 +2,7 @@
 include "pdo_sql.php";
 include "myPhpAPIs.php";
 
-$json = file_get_contents("20170610.json");
+$json = file_get_contents("./OpenData/TrainSchedules/20170610.json");
 // var_dump($json);
 
 $src_encoding = mb_detect_encoding($json, mb_list_encodings(), true); // not really working
@@ -14,24 +14,7 @@ echo gettype($root) . '<br>';
 
 myPrintObj('', $root["TrainInfos"], '<br>');
 
-// foreach ( $root["TrainInfos"] as $Obj ) {
-//         foreach( $Obj as $key => $val ) {
-//             myPrintObj($key, $val,'<br>');
-//             // if ( gettype($v) != "string") {
-//             //     // echo "{$key} ". gettype($v) . '<br>';
-//             //     var_dump($v);
-//             //     // $branch = json_decode($v);
 
-//             //     // foreach ( $v as $element) {
-//             //     //     echo "{$element}<br>";
-//             //     // }
-//             // } else {
-//             //     echo "{$key} : {$v}<br>";
-
-//             // }
-//         }
-//         echo '<hr>';
-// }
 
 ?>
 
--- main.php
+++ main.php
@@ -2,21 +2,50 @@
 
 include "pdo_sql.php";
 
-$date = '20170622';
+$date = '20170608';
 $train = '754';
+$depStation = '100';
+$arrStation = '158';
 
 $pdo = @new pdo($pdo_dsn, $db_user, $db_password, $pdo_opt);
-$sql = "SELECT date, train, depStation, arrStation, tickets FROM Tickets WHERE date = ? AND train = ?";
+$sql_tickets = "SELECT date, train, depStation, arrStation, tickets FROM Tickets WHERE date = ? AND depStation = ? AND arrStation = ?";
+$sql_stnInfo = "SELECT StnChtName, StnEngName FROM StationsInfo WHERE StnCode_3 = ?";
 
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$date, $train]);
+// $depStnInfo = array();
+// $arrStnInfo = array();
+
+$stmt = $pdo->prepare($sql_stnInfo);
+echo "Depart: {$depStation}".'<br>';
+$stmt->execute([$depStation]);
+$rs = $stmt->fetch(PDO::FETCH_ASSOC);
+// $rs = $stmt->fetch(PDO::FETCH_BOTH);
+foreach ($rs as $key => $val) {
+    echo "$key\t";
+}
+echo '<hr>';
+echo "Arrive: {$arrStation}".'<br>';
+$stmt->execute([$arrStation]);
+$rs = $stmt->fetch(PDO::FETCH_ASSOC);
+// $rs = $stmt->fetch(PDO::FETCH_BOTH);
+foreach ($rs as $key => $val) {
+    echo "$key: $val\t";
+}
+echo '<hr>';
+
+
+
+$stmt = $pdo->prepare($sql_tickets);
+// $stmt->execute([$date, $train]);
+$stmt->execute([$date, $depStation, $arrStation]);
+
 // var_dump($stmt);
-while ( $rs = $stmt->fetch(PDO::FETCH_BOTH) ) {
+while ( $rs = $stmt->fetch(PDO::FETCH_ASSOC) ) {
     foreach ($rs as $key => $val) {
         echo "$key: $val\t";
     }
     echo "<br>";
 
 }
+
 
 ?>
