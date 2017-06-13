bda26ddd54814a89c9804e6369d9e3a068e4e424
chiou <bohemianwind@gmail.com>
2017-06-13 08:24:47 +0800

    Debug: to avoid invalid submittion

--- main.php
+++ main.php
@@ -5,8 +5,8 @@ include "pdo_sql.php";
 if (isset($_POST['date'])) {
 
     $udate = $_POST['date'];
-    $depStn = $_POST['depStn'];
-    $arrStn = $_POST['arrStn'];
+    $depStnChtName = $_POST['depStn'];
+    $arrStnChtName = $_POST['arrStn'];
     $carClass = $_POST['carClass'];
 
     if ($udate == '') {
@@ -14,82 +14,91 @@ if (isset($_POST['date'])) {
     } else {
         echo $udate . '<br>';
     }
-    echo $depStn . '<br>';
-    echo $arrStn . '<br>';
+    // echo $depStn . '<br>';
+    // echo $arrStn . '<br>';
     // echo $carClass . '<br>';
-}
-echo '<hr>';
 
-$date = '20170608';
-$train = '754';
-// $depStation = '100';
-$depStation = '';
-// $arrStation = '158';
-$arrStation = '';
-$depStnChtName = '臺北';
-$arrStnChtName = '高雄';
-// $arrStnChtName = '內灣';
+    echo '<hr>';
+
+    $date = '20170608';
+    // $train = '754';
+    // $depStation = '100';
+    $depStation = '';
+    // $arrStation = '158';
+    $arrStation = '';
 
 
-$pdo = @new pdo($pdo_dsn, $db_user, $db_password, $pdo_opt);
-$sql_tickets = "SELECT date, train, depStation, arrStation, tickets FROM Tickets WHERE date = ? AND depStation = ? AND arrStation = ?";
-// $sql_stnInfo = "SELECT StnChtName, StnEngName FROM StationsInfo WHERE StnCode_3 = ?";
-$sql_stnInfo = "SELECT StnCode_3 FROM StationsInfo WHERE StnChtName = ?";
-
-// $depStnInfo = array();
-// $arrStnInfo = array();
-
-$stmt = $pdo->prepare($sql_stnInfo);
-$stmt->execute([$depStnChtName]);
-$rs = $stmt->fetch(PDO::FETCH_ASSOC);
-// $rs = $stmt->fetch(PDO::FETCH_BOTH);
-foreach ($rs as $key => $val) {
-    echo "$key: $val\t";
-    $depStation = $val; // should be only one
-}
-
-echo "Depart: {$depStnChtName}:{$depStation}".'<br>';
-echo '<hr>';
-
-
-$stmt->execute([$arrStnChtName]);
-$rs = $stmt->fetch(PDO::FETCH_ASSOC);
-// $rs = $stmt->fetch(PDO::FETCH_BOTH);
-foreach ($rs as $key => $val) {
-    echo "$key: $val\t";
-    $arrStation = $val; // should be also only one
-}
-
-echo "Arrive: {$arrStnChtName}:{$arrStation}".'<br>';
-echo '<hr>';
+    $pdo = @new pdo($pdo_dsn, $db_user, $db_password, $pdo_opt);
+    $sql_tickets = "SELECT date, train, depStation, arrStation, tickets FROM Tickets WHERE date = ? AND depStation = ? AND arrStation = ?";
+    // $sql_stnInfo = "SELECT StnChtName, StnEngName FROM StationsInfo WHERE StnCode_3 = ?";
+    $sql_stnInfo = "SELECT StnCode_3 FROM StationsInfo WHERE StnChtName = ?";
 
 
 
-$stmt = $pdo->prepare($sql_tickets);
-// $stmt->execute([$date, $train]);
-$stmt->execute([$date, $depStation, $arrStation]);
-
-// var_dump($stmt);
-
-// var_dump($stmt);
-$isFetched = false;
-while ( $rs = $stmt->fetch(PDO::FETCH_ASSOC) ) {
-
-    foreach ($rs as $key => $val) {
-        echo "$key: $val\t";
+    // To get StationCode_3 of departure station
+    $stmt = $pdo->prepare($sql_stnInfo);
+    $stmt->execute([$depStnChtName]);
+    $rs = $stmt->fetch(PDO::FETCH_ASSOC);
+    // $rs = $stmt->fetch(PDO::FETCH_BOTH);
+    $isDepStnFetched = false; // departure station found?
+    if ( $rs !== false ) {
+        foreach ($rs as $key => $val) {
+            echo "$key: $val\t";
+            $depStation = $val; // should be only one
+            $isDepStnFetched = true;
+        }
+    } else {
+        $isDepStnFetched = false;
     }
-    echo "<br>";
 
-    $isFetched = true;
+    echo "Depart: {$depStnChtName}:{$depStation}".'<br>';
+    echo '<hr>';
+
+    // To get StationCode_3 of destination station
+    $stmt->execute([$arrStnChtName]);
+    $rs = $stmt->fetch(PDO::FETCH_ASSOC);
+    // var_dump($rs);
+    // $rs = $stmt->fetch(PDO::FETCH_BOTH);
+    
+    $isArrStnFetched = false; // destination station found?
+    if ( $rs !== false) {
+        foreach ($rs as $key => $val) {
+            echo "$key: $val\t";
+            $arrStation = $val; // should be also only one
+            $isArrStnFetched = true;
+        }
+    } else {
+        $isArrStnFetched = false;
+    }
+
+    echo "Arrive: {$arrStnChtName}:{$arrStation}".'<br>';
+    echo '<hr>';
+
+    $isTrainFetched = false; // remaining tickets found?
+    if ( $isDepStnFetched && $isArrStnFetched ) {
+
+        $stmt = $pdo->prepare($sql_tickets);
+        $stmt->execute([$date, $depStation, $arrStation]);
+
+        while ( $rs = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+
+            foreach ($rs as $key => $val) {
+                echo "$key: $val\t";
+            }
+            echo "<br>";
+
+            $isTrainFetched = true;
+        }
+
+    }
+
+    if ($isTrainFetched) {
+        echo 'Got train'.'<br>';
+    } else {
+        echo 'No train'.'<br>';
+    }
+
 }
-
-if ($isFetched) {
-    echo 'Got train'.'<br>';
-} else {
-    echo 'No train'.'<br>';
-}
-
-
 
 ?>
 <html>
